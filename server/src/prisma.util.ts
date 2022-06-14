71c1ef487cc6004e1943df3722304dd1a5314498
Vikas Bhandary <vksbhandary@gmail.com>
2022-06-14 15:35:41 +0545

    fixing code smell

--- server/src/prisma.util.ts
+++ server/src/prisma.util.ts
@@ -14,17 +14,17 @@ export function isRecordNotFoundError(
 export async function transformStringFieldUpdateInput<
   T extends undefined | string | { set?: string }
 >(input: T, transform: (input: string) => Promise<string>): Promise<T> {
+  let response: T = input;
   if (typeof input === "object" && typeof input?.set === "string") {
-    return { set: await transform(input.set) } as T;
+    response = { set: await transform(input.set) } as T;
   }
   if (typeof input === "object") {
     if (typeof input.set === "string") {
-      return { set: await transform(input.set) } as T;
+      response = { set: await transform(input.set) } as T;
     }
-    return input;
   }
   if (typeof input === "string") {
-    return (await transform(input)) as T;
+    response = (await transform(input)) as T;
   }
-  return input;
+  return response;
 }
